3fea98d56a0b8b2448481964c3d19d57e2340a89
Aishwarya Janakiraman <aishwarya9425@gmail.com>
2023-09-18 16:44:21 +0530

    code setup

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,4 +1,4 @@
-import formatCurrency from ".../utils/helper";
+import { formatCurrency } from "../../utils/helpers";
 
 function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
